82d01cabe7480fc3962a027f8fc10f430cdf2957
DegtyarEO <geimerus@gmail.com>
2018-05-25 10:25:27 +0300

    hello world
    Signed-off-by: DegtyarEO <geimerus@gmail.com>

--- roll.js
+++ roll.js
@@ -1,4 +1,26 @@
 Draw.loadPlugin(function(ui) {
-    console.log(this);
-    console.log(Draw);
+// Adds resource for action
+   mxResources.parse('helloWorldAction=Hello, World!');
+
+   // Adds action
+   ui.actions.addAction('helloWorldAction', function() {
+       var ran = Math.floor((Math.random() * 100) + 1);
+       mxUtils.alert('A random number is ' + ran);
+   });
+
+   // Adds menu
+   ui.menubar.addMenu('Hello, World Menu', function(menu, parent) {
+       ui.menus.addMenuItem(menu, 'helloWorldAction');
+   });
+
+   // Reorders menubar
+   ui.menubar.container.insertBefore(ui.menubar.container.lastChild,
+       ui.menubar.container.lastChild.previousSibling.previousSibling);
+
+   // Adds toolbar button
+   ui.toolbar.addSeparator();
+   var elt = ui.toolbar.addItem('', 'helloWorldAction');
+
+   // Cannot use built-in sprites
+   elt.firstChild.style.backgroundImage = 'url(https://www.draw.io/images/logo-small.gif)';
 });
