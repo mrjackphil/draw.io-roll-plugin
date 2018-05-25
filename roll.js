74d21417d207fe14fb4baa56b25f593c7dd50df9
DegtyarEO <geimerus@gmail.com>
2018-05-25 11:40:42 +0300

    Basic done
    Signed-off-by: DegtyarEO <geimerus@gmail.com>

--- roll.js
+++ roll.js
@@ -1,26 +1,88 @@
 Draw.loadPlugin(function(ui) {
-// Adds resource for action
-   mxResources.parse('helloWorldAction=Hello, World!');
+    // Adds resource for rolling button
+       mxResources.parse('roll=Let\'s roll')
+    
+       let uiCreatePopupMenu = ui.menus.createPopupMenu;
+       ui.menus.createPopupMenu = function(menu, cell, evt)
+       {
+           uiCreatePopupMenu.apply(this, arguments);
+           
+           let graph = ui.editor.graph;
+           if (graph.model.isVertex(graph.getSelectionCell()))
+           {
+               this.addMenuItems(menu, ['-', 'roll'], null, evt);
+           }
+       };
+    
+        ui.actions.addAction('roll', function()
+        {
+           let value = ui.editor.graph.getSelectionCell().value;
+           alert(roll(value));
+        });
+    
+    function roll(roll,output) {
+        let dice;
+        let count;
+        let arr = [];
+        let sum = 0;
+        let max = 99999;
 
-   // Adds action
-   ui.actions.addAction('helloWorldAction', function() {
-       var ran = Math.floor((Math.random() * 100) + 1);
-       mxUtils.alert('A random number is ' + ran);
-   });
+        count = roll.indexOf('d');
+        count = roll.slice(0,count);
+        dice  = roll.replace(count+"d","");
 
-   // Adds menu
-   ui.menubar.addMenu('Hello, World Menu', function(menu, parent) {
-       ui.menus.addMenuItem(menu, 'helloWorldAction');
-   });
+        if (dice.indexOf("+") !== -1 || dice.indexOf("-") !== -1){
+            let diceWithMod = dice;
 
-   // Reorders menubar
-   ui.menubar.container.insertBefore(ui.menubar.container.lastChild,
-       ui.menubar.container.lastChild.previousSibling.previousSibling);
 
-   // Adds toolbar button
-   ui.toolbar.addSeparator();
-   var elt = ui.toolbar.addItem('', 'helloWorldAction');
+            let plus = dice.indexOf("+");
+            let minus = dice.indexOf("-");
+            if (plus === -1){plus = max};
+            if (minus === -1){minus = max};
 
-   // Cannot use built-in sprites
-   elt.firstChild.style.backgroundImage = 'url(https://www.draw.io/images/logo-small.gif)';
-});
+            if (plus < minus){
+                dice = dice.slice(0, dice.indexOf("+"));
+            }else{
+                dice = dice.slice(0, dice.indexOf("-"));
+            }
+        }
+
+        for (let i=0; i < count; i++){
+            arr[i] = Math.floor(Math.random() * dice + 1);
+            sum += arr[i];
+        }
+
+        let r = count+"d"+dice;
+        let l = roll.replace(r,"");
+        l = addbits(l);
+        sum += l;  
+
+        let message = "" +
+        "You roll: " + sum + 
+        "\nDices results: " + arr +
+        "\nModificators: " + l;
+
+        if (typeof output !== "undefined"){
+           switch (output){
+              case "sum":
+               return sum;
+              case "arr":
+               return arr;
+              case "r":
+               return r;
+              case "l":
+               return l;
+           }                                 
+        }
+
+        return message;
+    };
+
+    function addbits(s){
+        let total= 0, s= s.match(/[+\-]*(\.\d+|\d+(\.\d+)?)/g) || [];
+        while(s.length){
+            total+= parseFloat(s.shift());
+        }
+        return total;
+    }
+})
